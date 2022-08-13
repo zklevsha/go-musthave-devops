9ace078d5e86e35c81fe517deab3ef42dc55bce8
Kirill Zhukov <kdzhukov@sbercloud.ru>
2022-08-13 18:25:23 +0300

    added tests for poller

--- internal/poller/poller_test.go
+++ internal/poller/poller_test.go
@@ -1,7 +1,10 @@
 package poller
 
 import (
+	"context"
+	"sync"
 	"testing"
+	"time"
 )
 
 func BenchmarkGetRtmMetrics(b *testing.B) {
@@ -20,4 +23,53 @@ func BenchmarkGetCPUMetrics(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		getCPUMetrics()
 	}
+}
+
+func TestCUint64(t *testing.T) {
+	name := "testing cUint64"
+	var value uint64 = 1
+	t.Run(name, func(t *testing.T) {
+		cUint64(value)
+	})
+
+}
+
+func TestGetRtmMetrics(t *testing.T) {
+	name := "testing getRtmMetrics"
+	t.Run(name, func(t *testing.T) {
+		getRtmMetrics()
+	})
+}
+
+func TestGetMemoryMetrics(t *testing.T) {
+	name := "testing getMemoryCPUMetrics"
+	t.Run(name, func(t *testing.T) {
+		_, err := getMemoryMetrics()
+		if err != nil {
+			t.Errorf("getMemoryMetrics() has returned an error: %s", err)
+		}
+	})
+}
+
+func TestGetCPUMetrics(t *testing.T) {
+	name := "testing getCPUMetrics"
+	t.Run(name, func(t *testing.T) {
+		_, err := getCPUMetrics()
+		if err != nil {
+			t.Errorf("getCPUMetrics() has returned an error: %s", err)
+		}
+	})
+}
+
+func TestPoll(t *testing.T) {
+	ctxTimeout, cancel := context.WithTimeout(context.Background(), time.Second*3)
+	defer cancel()
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	name := "testing Pool"
+	t.Run(name, func(t *testing.T) {
+		Poll(ctxTimeout, &wg, time.Second)
+	})
+
 }
